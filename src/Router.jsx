3684b946952511c24ec2215aabdac683f9a23894
Angelo Lese <angelo.lese@hotmail.com>
2023-04-03 09:25:15 -0300

    exercicio 4 um pouco de context ok

--- src/Router.jsx
+++ src/Router.jsx
@@ -9,14 +9,20 @@
  */
 
 import { createBrowserRouter } from "react-router-dom";
+import { QuemSou } from "./pages/QuemSou";
+import { Home } from "./pages/Home";
+
 import { App } from "./App";
+
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      //  as rotas serão incluídas aqui!
+     { path: "/home", element: < Home />},
+     { path: "/quem-sou", element: <QuemSou /> },
+     
     ],
   },
 ]);
