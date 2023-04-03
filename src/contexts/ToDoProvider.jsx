8647891435e41866c6d71938e054d16853965b38
Angelo Lese <angelo.lese@hotmail.com>
2023-04-03 10:12:13 -0300

    exercicio 05 ok

--- src/contexts/ToDoProvider.jsx
+++ src/contexts/ToDoProvider.jsx
@@ -1,3 +1,12 @@
 import { createContext } from "react";
 
 export const todoContext = createContext();
+
+
+export const ToDoProvider =  (props) => {
+    return(
+<todoContext.Provider value={undefined}>
+    {props.children}</todoContext.Provider>
+    );
+};
+
